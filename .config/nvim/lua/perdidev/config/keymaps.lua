fa03826bd503e57bff2dc0d891ae35753515e4a0
perdidev <perdidev@gmail.com>
2025-10-16 19:43:11 +0800

    update: new feat for nvim config

--- .config/nvim/lua/perdidev/config/keymaps.lua
+++ .config/nvim/lua/perdidev/config/keymaps.lua
@@ -6,3 +6,4 @@ local map = vim.api.nvim_set_keymap
 
 map("n", "<leader>e", ":Ex<Cr>", { desc = "To menu explore", silent = true})
 map("n", "<leader>qq", ":quit<Cr>", { desc = "Quit from project", silent = true })
+map("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show diagnostic", silent = true })
--- .config/nvim/lua/perdidev/plugins/theme.lua
+++ .config/nvim/lua/perdidev/plugins/theme.lua
@@ -15,6 +15,9 @@ return {
             snacks = {
                 enabled = false,
                 indent_scope_color = "lavender",
+            },
+            native_lsp = {
+                enabled = true
             }
         }
     },
