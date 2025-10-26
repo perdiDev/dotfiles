7b546f5ef92e1a0e79e8492f036ac6c302701785
perdidev <perdidev@gmail.com>
2025-10-26 21:09:02 +0800

    update(nvim): update lualine

--- .config/nvim/lua/perdidev/plugins/lualine.lua
+++ .config/nvim/lua/perdidev/plugins/lualine.lua
@@ -2,10 +2,13 @@ return {
     'nvim-lualine/lualine.nvim',
     opts = {
         options = {
-            section_separators = '', component_separators = '',
+            section_separators = { left = '', right = '' },
+            component_separators = { left = '', right = '' },
+            -- section_separators = '', component_separators = '',
             theme = 'palenight',
             sections = {
-                lualine_a = {}
+                lualine_a = {'mode'},
+                lualine_b = {'searchcount'}
             }
         }
     }
