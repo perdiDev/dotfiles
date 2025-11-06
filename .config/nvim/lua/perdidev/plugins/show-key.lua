6ad104e7dea8f5a002fc5d51b9e83405c32bda62
perdidev <perdidev@gmail.com>
2025-11-06 19:40:53 +0800

    feat(nvim): show key plugin nvim to show key when typing in nvim

---
+++ .config/nvim/lua/perdidev/plugins/show-key.lua
@@ -0,0 +1,25 @@
+return {
+  "nvzone/showkeys",
+  cmd = "ShowkeysToggle",
+  opts = {
+    timeout = 1,
+    maxkeys = 3,
+    position = "top-right",
+    exclude_mode = {"insert"},
+
+
+    keyformat = {
+      ["<BS>"] = "󰁮 ",
+      ["<CR>"] = "󰘌",
+      ["<Space>"] = "󱁐",
+      ["<Up>"] = "󰁝",
+      ["<Down>"] = "󰁅",
+      ["<Left>"] = "󰁍",
+      ["<Right>"] = "󰁔",
+      ["<PageUp>"] = "Page 󰁝",
+      ["<PageDown>"] = "Page 󰁅",
+      ["<M>"] = "Alt",
+      ["<C>"] = "Ctrl",
+    },
+  }
+}
