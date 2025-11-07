b689c5cbc42c375a5245c4138a27f35e1be28b90
perdidev <perdidev@gmail.com>
2025-11-07 19:25:15 +0800

    update(nvim): adding tab config

--- .config/nvim/lua/perdidev/plugins/lualine.lua
+++ .config/nvim/lua/perdidev/plugins/lualine.lua
@@ -1,5 +1,14 @@
 local colors = require("perdidev.config.pallates")
 
+local project = function()
+    local dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+    return dir
+end
+
+local tabcount = function()
+    return vim.fn.tabpagenr("$")
+end
+
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -17,18 +26,41 @@ return {
                 fmt = string.lower,
                 section_separators   = { left = "", right = "" },
                 component_separators = { left = "", right = "" },
+                always_show_tabline = false,
                 globalstatus = true,
             },
+
             sections = {
                 lualine_a = {
                     {
-                        "filename",
+                        project,
+                        file_status = false,
+                        draw_empty = true,
                         separator = { left = "  " },
+                        color = {bg = colors.red, fg = colors.base, gui = 'bold'},
+                    },
+                    {
+                        "filename",
+                        cond = function()
+                            if tabcount() > 1 then
+                                return false
+                            else
+                                return true
+                            end
+                        end,
+                        symbols = {
+                            modified = "󰐾",
+                            readonly = "",
+                            unnamed = "",
+                            newfile = "",
+                        },
+                        icon = "",
                         color = {bg = colors.red, fg = colors.base, gui = 'bold'},
                     },
                     {
                         "branch",
                         icon = "",
+                        draw_empty = true,
                         separator = { right = "" },
                         color = {bg = colors.red, fg = colors.base},
                     }
@@ -49,13 +81,17 @@ return {
                             removed = " ",
                         },
                     },
-                    {
-                        "buffer",
-                        hide_filename_extension = true,
-                        mode = 3,
-                    },
                 },
+
                 lualine_x = {
+                    {
+                        "diagnostics"
+                    },
+                    {
+                        "filetype",
+                        colored = false,
+                        icon_only = true,
+                    },
                     {
                         'lsp_status',
                         icon = "",
@@ -65,7 +101,7 @@ return {
                         },
                         ignore_lsp = {},
                         show_name = true,
-                    }
+                    },
                 },
                 lualine_y = {
                     {
@@ -77,14 +113,42 @@ return {
                 lualine_z = {
                     {
                         "progress",
+                        draw_empty = true,
                         separator = { left = ""}
                     },
                     {
                         "mode",
+                        draw_empty = true,
                         separator = { right = "  "},
                         color = { fg = colors.crust, gui = 'bold' }
                     }
                 },
+            },
+
+            tabline = {
+                lualine_a = {
+                    {
+                        "tabs",
+                        mode = 1,
+                        path = 1,
+                        separator = { right = "" },
+                        tabs_color = {
+                            active = { bg = colors.rosewater, fg = colors.base },
+                            inactive = { bg = colors.overlay0, fg = colors.base }
+                        },
+                        symbols = {
+                            modified = "󰐾"
+                        },
+                        fmt = function (name)
+                            return " " .. name
+                        end,
+                    },
+                },
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {}
             },
         }
     end
