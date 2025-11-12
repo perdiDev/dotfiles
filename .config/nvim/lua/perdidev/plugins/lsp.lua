f4943a593069a558c42d6e0b1555e41e3bc9df38
perdidev <perdidev@gmail.com>
2025-11-12 22:00:33 +0800

    update(nvim): reconfigure lsp config
    -restructuring the configuration and make it understable

--- .config/nvim/lua/perdidev/plugins/lsp.lua
+++ .config/nvim/lua/perdidev/plugins/lsp.lua
@@ -1,14 +1,16 @@
 return {
     {
         "mason-org/mason.nvim",
-        opts = {}
+        opts = {
+            ui = {
+                border = "rounded",
+                width = 0.8,
+                height = 0.8
+            }
+        }
     },
     {
         "mason-org/mason-lspconfig.nvim",
-        dependencies = {
-            "mason.nvim",
-            "neovim/nvim-lspconfig"
-        },
         opts = {
             ensure_installed = {
                 "lua_ls",
@@ -18,16 +20,39 @@ return {
                 "ts_ls",
                 "html"
             },
+            automatic_enable = true
         },
     },
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            'saghen/blink.cmp',
+            'mason.nvim',
+            'mason-org/mason-lspconfig.nvim',
+        },
         config = function()
+            local lspServer = require('mason-lspconfig').get_installed_servers()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+            for _, server in pairs(lspServer) do
+                vim.lsp.config(server, {
+                    capabilities = capabilities
+                })
+            end
+
             vim.lsp.config('lua_ls', {
                 settings = {
                     Lua = {
-                        diagnostics = {
-                            globals = {"vim"},
+                        runtime = {
+                            version = "LuaJIT",
+                        },
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                [vim.fn.stdpath("data") .. "/lazy"] = true,
+                                [vim.fn.stdpath("config") .. "/lua"] = true, -- your own config
+                            }
                         },
                         telemetry = { enable = false }
                     }
