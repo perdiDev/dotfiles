225773e321450f7be2345472861c2b859b686c05
perdidev <perdidev@gmail.com>
2026-08-10 02:14:42 +0800

    hypr: add idle inhibitor

--- .config/hypr/conf/autostart.lua
+++ .config/hypr/conf/autostart.lua
@@ -6,5 +6,6 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("dbus-update-activation-environment")
     hl.exec_cmd("hyprpaper & waybar & hypridle & ghostty")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
+    hl.exec_cmd("wayland-pipewire-idle-inhibit")
 end)
 
